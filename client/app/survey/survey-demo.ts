ed15b71b54535638bcacc2a426257b9cf640c583
Marcin Wachowiak <marcin.wachowiak@mobica.com>
2016-03-31 15:39:29 +0200

    alternative form validation

--- client/app/survey/survey-demo.ts
+++ client/app/survey/survey-demo.ts
@@ -1,54 +1,88 @@
 import {Component} from 'angular2/core';
+import { Control } from "angular2/common";
 
-import {Survey} from './survey';
-import {QuestionModel} from './question-model';
-import {TextboxQuestion} from './textbox-question';
-import {DropDownQuestion} from './dropdown-question';
+import {
+	FormBuilder,
+	Validators,
+	Control,
+	ControlGroup,
+	FORM_DIRECTIVES
+} from 'angular2/common';
+
+interface ValidationResult{
+   [key:string]:boolean;
+}
+
+export class UsernameValidator {
+
+    static startsWithNumber(control: Control): ValidationResult { 
+    
+      if ( control.value !="" && !isNaN(control.value.charAt(0)) ){
+        return {"startsWithNumber": true};
+      }
+    
+      return null;
+    }
+   
+    static usernameTaken(control: Control): Promise<ValidationResult> {
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === "David") {
+                    resolve({"usernameTaken": true})
+                } else {
+                    resolve(null);
+                };
+                
+            }, 1000);
+        });
+
+    }
+}
+
+
 
 @Component({
-    selector:'survey-demo',
-    template:'<div><h1>Survey using Dynamic Form</h1><survey [model]="questionModel"></survey></div>',
-    directives:[Survey]
+	template: `
+		<form [ngFormModel]="form">
+			<input type="text" ngControl="username" />
+			
+			<p *ngIf="username.pending">Fetching data from the server...</p>
+			
+			<div *ngIf="username.dirty && !username.valid && !username.pending">
+			  <p *ngIf="username.errors.required">Username is required.</p>
+			  <p *ngIf="username.errors.startsWithNumber">Your username can't start with a number</p>
+			  <p *ngIf="username.errors.usernameTaken">This username is taken</p>
+			</div>
+			
+			<button (click)="submitData()" [disabled]="!form.valid" class="btn btn-primary">Sumbit data</button>
+		</form>
+	`,
+	directives: [FORM_DIRECTIVES]
 })
 
 export class SurveyDemo {
 
-    questionModel = new QuestionModel();
-
-    constructor(){
-
-        let question = new TextboxQuestion();
-        question.key = 'lastName';
-        question.text = 'Last name';
-        question.required = true;
-        question.order = 2;
-        this.questionModel.questions.push(question);
-
-        question = new TextboxQuestion();
-        question.key = 'firstName';
-        question.text = 'First name';
-        question.required = true;
-        question.order = 1;
-        this.questionModel.questions.push(question);
-
-        question = new TextboxQuestion();
-        question.key = 'emailAddress';
-        question.text = 'Email';
-        question.required = false;
-        question.type = 'email';
-        question.order = 3;
-        this.questionModel.questions.push(question);
-
-        let ddQuestion = new DropDownQuestion();
-        ddQuestion.key = 'country';
-        ddQuestion.text = 'Country';
-        ddQuestion.options.push({key:'usa',value:'USA'});
-        ddQuestion.options.push({key:'germany',value:'Germany'});
-        ddQuestion.options.push({key:'canada',value:'Canada'});
-        ddQuestion.options.push({key:'australia',value:'Australia'});
-        ddQuestion.order = 4;
-        this.questionModel.questions.push(ddQuestion);
-
-        this.questionModel.questions.sort((a,b) => a.order - b.order);
+	form: ControlGroup;
+	
+	username: Control;
+	email: Control;
+	asyncEmail: Control;
+	
+	constructor(private builder: FormBuilder) {
+		
+		this.username = new Control(
+			"", 
+			Validators.compose([Validators.required, UsernameValidator.startsWithNumber]),
+			UsernameValidator.usernameTaken
+		);
+		
+		this.form = builder.group({
+			username:  this.username
+		});
+	}	
+	
+	submitData(){
+     	console.log(JSON.stringify(this.form.value))
     }
 }
